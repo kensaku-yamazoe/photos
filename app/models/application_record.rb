cf8a10a9a988d9fe13dba3f17bdafc41dd7c29d3
kensaku33 <picow.zoe33@gmail.com>
2019-10-21 19:22:02 +0900

    名前保存されない

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
+
+  # mount_uploader :image, ImageUploader
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,11 @@
 class Photo < ApplicationRecord
 
   belongs_to :user
-  validates :image, :title, presence: true
+  has_many :images, :dependent => :destroy
+  accepts_nested_attributes_for :images
+
+  validates :title, presence: true
+
 
   # mount_uploader :image, ImageUploader
   
