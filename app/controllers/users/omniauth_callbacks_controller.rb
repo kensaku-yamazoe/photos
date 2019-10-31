b099b4ba8f6f9ae9a500826593d7fd742bb9ad4c
kensaku33 <picow.zoe33@gmail.com>
2019-10-31 16:52:06 +0900

    google

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,20 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  
+  def google_oauth2
+    @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
+
+    # 保存済みかどうかのチェック
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      session["devise.google_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+  
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_130755) do
+ActiveRecord::Schema.define(version: 2019_10_31_064639) do
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "photo_id", null: false
@@ -56,6 +56,9 @@ ActiveRecord::Schema.define(version: 2019_10_26_130755) do
     t.integer "camera", default: 0
     t.integer "lens", default: 0
     t.string "url", default: ""
+    t.string "provider"
+    t.string "uid"
+    t.string "token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
